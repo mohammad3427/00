ba2c92d7570c6e69f13c6770c43c617e70dacc50
S07C02 <S07C02@S07C02>
2024-05-01 11:04:16 +0330

    mmd

--- 00/Program.cs
+++ 00/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, !");
+Console.WriteLine("123");
+Console.WriteLine("hi");
+Console.WriteLine("boy");
 
